65a5015b151a3d12eb9d7c85f9009eaebba0a237
colincheungkm <colincheung1993hk@gmail.com>
2023-06-29 20:44:37 -0400

    update

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import { db } from './firebase';
 import { query, collection, onSnapshot, updateDoc, doc, addDoc, deleteDoc } from 'firebase/firestore';
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#3F80ED] to-[#1CB5E0]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-lg shadow-xl p-4`,
   heading: `text-3xl font-semibold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
